21aac8af1f90c56f90dffb092cbd8e740a047b1e
Jchen7519 <Jchen7519@bths.edu>
2022-11-28 22:54:19 -0500

    prevent undefined in array

--- index.js
+++ index.js
@@ -28,22 +28,28 @@ function deleteGrocery()
 function moveUpList()
 {
     recordData()
-    idx = numVal - 1
-    aboveIdx = idx - 1
-    aboveVal = groceryArr[aboveIdx]
-    atVal = groceryArr[idx]
-    groceryArr.splice(aboveIdx, 2, atVal, aboveVal)
-    // groceryArr.push(firstVal)
-    rewriteList()
+    if (numVal > 1 && numVal <= groceryArr.length)
+    {
+        idx = numVal - 1
+        aboveIdx = idx - 1
+        aboveVal = groceryArr[aboveIdx]
+        atVal = groceryArr[idx]
+        groceryArr.splice(aboveIdx, 2, atVal, aboveVal)
+        // groceryArr.push(firstVal)
+        rewriteList()
+    }
 }
 function moveDownList()
 {
     recordData()
-    idx = numVal - 1
-    belowIdx = idx + 1
-    belowVal = groceryArr[belowIdx]
-    atVal = groceryArr[idx]
-    groceryArr.splice(idx, 2, belowVal, atVal)
-    // groceryArr.push(firstVal)
-    rewriteList()
+    if (numVal >= 1 && numVal < groceryArr.length)
+    {
+        idx = numVal - 1
+        belowIdx = idx + 1
+        belowVal = groceryArr[belowIdx]
+        atVal = groceryArr[idx]
+        groceryArr.splice(idx, 2, belowVal, atVal)
+        // groceryArr.push(firstVal)
+        rewriteList()
+    }
 }
