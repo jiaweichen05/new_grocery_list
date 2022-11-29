ff8d8078b56c705e2879b0229ff6ea4afb367204
Jchen7519 <Jchen7519@bths.edu>
2022-11-28 22:32:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+function recordData()
+{
+    groceryVal = form.groceryInput.value
+    numVal = parseInt(form.numInput.value)
+}
+function rewriteList()
+{
+    shoppingList.innerHTML = ""
+    for (let i = 0; i < groceryArr.length; i++)
+    {
+        listDisplay = i + 1
+        shoppingList.innerHTML += "<p>" + listDisplay + ": " + groceryArr[i] + "</p>"
+    }
+}
+function addGrocery()
+{
+    recordData()
+    groceryArr.push(groceryVal)
+    rewriteList()
+}
+function deleteGrocery()
+{
+    recordData()
+    idx = numVal - 1
+    groceryArr.splice(idx, 1)
+    rewriteList()
+}
+function moveUpList()
+{
+    recordData()
+    idx = numVal - 1
+    aboveIdx = idx - 1
+    aboveVal = groceryArr[aboveIdx]
+    atVal = groceryArr[idx]
+    groceryArr.splice(aboveIdx, 2, atVal, aboveVal)
+    // groceryArr.push(firstVal)
+    rewriteList()
+}
+function moveDownList()
+{
+    recordData()
+    idx = numVal - 1
+    belowIdx = idx + 1
+    belowVal = groceryArr[belowIdx]
+    atVal = groceryArr[idx]
+    groceryArr.splice(idx, 2, belowVal, atVal)
+    // groceryArr.push(firstVal)
+    rewriteList()
+}
